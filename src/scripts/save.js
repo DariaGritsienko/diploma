41b5e61175780d2788dd6d85925ba6ab51d9e3bd
Дарья Грициенко <60083638@leroymerlin.ru>
2020-03-17 17:43:55 +0300

    delete unnecessary file

--- src/scripts/save.js
+++
@@ -1,6 +0,0 @@
-// Сохранение данных в sessionStorage
-sessionStorage.setItem('key', 'value');
-
-// Получение данных из sessionStorage
-const data = sessionStorage.getItem('key');
-console.log(data);
