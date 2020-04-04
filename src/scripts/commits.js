2af2963affbf50d4bc1c93742ff36351806077c3
Daria Gritsienko <dariagritsienko@yandex.ru>
2020-04-05 01:16:14 +0300

    fixed bug in slider

--- src/scripts/commits.js
+++ src/scripts/commits.js
@@ -1,22 +1,24 @@
 class CommitsLoader{
-    constructor(api, commitsList){
+    constructor(api, commitsList, swiper){
         this.api = api;
         this.commitsList = commitsList;
+        this.swiper = swiper;
     }
     loadData(){
         this.api.getCommit()
-        .then((result) => {
-            this.commitsList.renderCommit(result)
-        })
-        .catch((err) => {
-            console.error(`Ошибка: ${err}`);
-        })
+            .then((result) => {
+                this.commitsList.renderCommit(result);
+            })
+            .catch((err) => {
+                console.error(`Ошибка: ${err}`);
+            })
     }
 }
 
 import {api} from './api'
 import {commitsList} from './cardList'
+import {swiper} from './swiper'
 
-export const commits = new CommitsLoader(api, commitsList);
+export const commits = new CommitsLoader(api, commitsList, swiper);
 
 commits.loadData();
